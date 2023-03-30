cd192b814c5f052d4bcba6d5bd9c32a97e23628b
Valerii Viun <2455521@gmail.com>
2023-03-30 16:20:21 +0300

    intermediate code

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items";
+// Change code below this line
+
+// создаємо елементи img
+
+// function createGalleryItem(galleryItems) {
+//     return galleryItems.map(({ preview, original, description }) => {
+//         return `<div class="gallery__item">
+//           <a class="gallery__link" href="${original}">
+//             <img class="gallery__image"
+//               src="${preview}"
+//               data-source="${original}"
+//               alt="${description}" /> </a> </div>`;
+//       })
+//       .join("");
+//   }
+
+
+
+// // добавляємо улементы img в HTML
+// const gallery = document.querySelector(".gallery");
+
+// function creatMarkup() {
+//   let markup = "";
+//   for (let i = 0; i < 9; i += 1) {
+//     markup += `<li class="gallery__item">
+//     //           <a class="gallery__link" href="${original}">
+//     //             <img class="gallery__image"
+//     //               src="${preview}"
+//     //               data-source="${original}"
+//     //               alt="${description}" /> </a> </li>`;
+//   }
+//   content.innerHTML = markup;}
+// creatMarkup();
+// content.addEventListener('click', onclick);
+
+// function onclick(evt){
+//     if(!evt.target.classList.contains('js-target')){
+//    return;     
+//     }
+//     console.log(evt.target);
+// }
+
+
+
+
+
+
+// console.log(galleryItems);
